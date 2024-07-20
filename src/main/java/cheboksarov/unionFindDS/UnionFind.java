d17d1ce1da5675d9feb2854ecd9fc90c5999824d
Cheboksarov Yaroslav <yacheboksarov768@gmail.com>
2024-07-20 12:40:04 +0300

    Added some counters: size, compCount, array compSize - to keep track of the number of nodes in each component, also added some methods to use these counters.

--- src/main/java/cheboksarov/unionFindDS/UnionFind.java
+++ src/main/java/cheboksarov/unionFindDS/UnionFind.java
@@ -3,14 +3,25 @@ package cheboksarov.unionFindDS;
 import java.util.*;
 
 public class UnionFind implements IUnionFind{
-    private int[] parent;
+    private final int[] parent;
+    // Кол-во нод в union find
+    private final int size;
+    // Кол-во нод в каждой компоненте
+    private final int[] compSize;
+    // Кол-во компонент
+    private int compCount;
 
     public UnionFind(int size) {
+        if(size <= 0) throw new IllegalArgumentException("The size must be greater than 0");
         parent = new int[size];
+        compSize = new int[size];
       //Изначально каждая нода является корневой, так как указывает на саму себя
         for (int i = 0; i < size; i++) {
             parent[i] = i;
+            compSize[i] = 1;
         }
+        this.size = size;
+        this.compCount = size;
     }
     @Override
     public int find(int node) {
@@ -20,23 +31,32 @@ public class UnionFind implements IUnionFind{
 
     @Override
     public void union(int node1, int node2) {
+        // Если ноды уже в одной компоненте, то их не нужно объединять
+        if(isConnected(node1, node2)) return;
         int root1 = find(node1);
         int root2 = find(node2);
-        int compSize1 = sizeOfComponent(root1);
-        int compSize2 = sizeOfComponent(root2);
-        if(compSize1 >= compSize2){
-            parent[root2] = root1;
-        }else{
-            parent[root1] = root2;
+        int compSize1 = compSize[root1];
+        int compSize2 = compSize[root2];
+        // Определить какая нода останется корнейвой - parent, а какая станет дочерней - child
+        int parentNode = root1;
+        int childNode = root2;
+        if(compSize1 < compSize2) {
+            parentNode = root2;
+            childNode = root1;
         }
+        parent[childNode] = parentNode;
+        compSize[parentNode] = compSize1 + compSize2;
+        // compSize[node] == 0 <-- значит, что node не является корневой
+        compSize[childNode] = 0;
+        compCount--;
     }
-    private int sizeOfComponent(int root){
-        return getComponent(root).size();
-    }
+
     public List<List<Integer>> getAllComponents(){
         List<List<Integer>> components = new ArrayList<>();
-        for(int root: getAllRoots()){
-            components.add(getComponent(root));
+        for(int i = 0; i < size; i++){
+            if(compSize[i] > 0){
+                components.add(getComponent(i));
+            }
         }
         return components;
     }
@@ -51,17 +71,18 @@ public class UnionFind implements IUnionFind{
         }
         return component;
     }
-    public List<Integer> getAllRoots(){
-        int lastRoot = -1;
-        List<Integer> roots = new ArrayList<>();
-        for(int i: parent){
-            int root = find(i);
-            if(root!= lastRoot){
-                roots.add(root);
-                lastRoot = root;
-            }
-        }
-        return roots;
+    public int[] getParent(){
+        return parent;
+    }
+    // Проверка на то, что две ноды принадлежат одной компоненте
+    public boolean isConnected(int node1, int node2) {
+        return find(node1) == find(node2);
+    }
+    public int componentSize(int root){
+        return compSize[root];
+    }
+    public int size(){
+        return size;
     }
 
     @Override
--- src/test/java/UnionFindTest.java
+++ src/test/java/UnionFindTest.java
@@ -1,6 +1,10 @@
 import cheboksarov.unionFindDS.UnionFind;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnionFindTest {
@@ -21,6 +25,20 @@ public class UnionFindTest {
         uf.union(1, 4);
         System.out.println(uf.getAllComponents().toString());
         System.out.println(uf.find(0));
+        System.out.println(Arrays.toString(uf.getParent()));
         assertEquals(2, uf.find(1));
+    }
+
+    @Test
+    public void unionNodesAlreadyInTheSameComponentTest(){
+        UnionFind uf = new UnionFind(5);
+        uf.union(0, 1);
+        uf.union(2, 1);
+        uf.union(0, 2);
+        System.out.println(uf.getAllComponents().toString());
+        System.out.println(uf.find(0));
+        System.out.println(Arrays.toString(uf.getParent()));
+        System.out.println(uf.componentSize(0));
+        assertEquals(List.of(0, 1, 2), uf.getComponent(2));
     }
 }
